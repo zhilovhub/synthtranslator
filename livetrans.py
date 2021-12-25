3fad662d49d120de2c4126b39d888691ef26d61b
Ilya Zhilov <zhilov702@gmail.com>
2021-12-25 10:20:29 +0300

    added standard funcs to classes

--- livetrans.py
+++ livetrans.py
@@ -12,14 +12,83 @@ from settings import CHUNKS, SAMPLE_FORMAT, CHANNELS, FS, SECONDS
 
 
 class SynthTranslator:
+    """Перевод, распознавание, синтез"""
     def __init__(self):
         self.folder_id = FOLDER_ID
         self.api_key = API_KEY
 
+    def recognize(self, audio):
+        with open(audio, "rb") as f:
+            data = f.read()
+
+        params = "&".join([
+            "topic=general",
+            "folderId=%s" % self.folder_id,
+            "lang=ru-RU",
+            "format=lpcm",
+            "sampleRateHertz=16000"
+        ])
+
+        url = urllib.request.Request("https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?%s" % params, data=data)
+        url.add_header("Authorization", "Api-Key %s" % self.api_key)
+
+        response_data = urllib.request.urlopen(url).read().decode('UTF-8')
+        decoded_data = json.loads(response_data)
+
+        if decoded_data.get("error_code") is None:
+            result = decoded_data.get("result")
+        else:
+            result = 'Не распознал'
+
+        return result
+
+    def translate(self, text):
+        headers = {
+            "Authorization": f"Api-Key {self.api_key}",
+        }
+
+        # Отправим запрос
+        res = requests.post(
+            "https://translate.api.cloud.yandex.net/translate/v2/translate",
+            json={
+                "sourceLanguageCode": 'ru',
+                "targetLanguageCode": 'en',
+                "format": 'PLAIN_TEXT',
+                "texts": text
+            },
+            headers=headers)
+
+        return json.loads(res.text)['translations'][0]['text']
+
+    def synthesize(self, text):
+        url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
+        headers = {
+            "Authorization": f"Api-Key {self.api_key}",
+        }
+
+        data = {
+            'text': text,
+            'lang': 'en-US',
+            'folderId': self.folder_id,
+            'voice': 'nick',
+            'speed': 1.2,
+            'format': 'lpcm',
+            'sampleRateHertz': 48000,
+        }
+
+        with requests.post(url, headers=headers, data=data, stream=True) as resp:
+            if resp.status_code != 200:
+                raise RuntimeError("Invalid response received: code: %d, message: %s" % (resp.status_code, resp.text))
+
+            for chunk in resp.iter_content(chunk_size=None):
+                yield chunk
+
 
 class Recorder:
-    def __init__(self):
+    """Запись голоса"""
+    def __init__(self, synth_translator):
         self.count = 0
+        self.synth_translator = synth_translator
 
     def record(self):
         # сделать запись нормальной
@@ -67,100 +136,35 @@ class Recorder:
         sound = AudioSegment.from_wav(from_file)
         sound.export(to_file, format='s16le', bitrate='16k')
 
-        main()
+        main_loop(self.synth_translator)
 
 
 filename = 'output.wav'
 
 
-def synthesize(text):
-    url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
-    headers = {
-        "Authorization": f"Api-Key {API_KEY}",
-    }
+def main_loop(synth_translator: SynthTranslator):
+    result = synth_translator.recognize('output.pcm')
 
-    data = {
-        'text': text,
-        'lang': 'en-US',
-        'folderId': FOLDER_ID,
-        'voice': 'nick',
-        'speed': 1.2,
-        'format': 'lpcm',
-        'sampleRateHertz': 48000,
-    }
-
-    with requests.post(url, headers=headers, data=data, stream=True) as resp:
-        if resp.status_code != 200:
-            raise RuntimeError("Invalid response received: code: %d, message: %s" % (resp.status_code, resp.text))
-
-        for chunk in resp.iter_content(chunk_size=None):
-            yield chunk
-
-
-def translate(text):
-    headers = {
-        "Authorization": f"Api-Key {API_KEY}",
-    }
-
-    # Отправим запрос
-    res = requests.post(
-        "https://translate.api.cloud.yandex.net/translate/v2/translate",
-        json={
-            "sourceLanguageCode": 'ru',
-            "targetLanguageCode": 'en',
-            "format": 'PLAIN_TEXT',
-            "texts": text
-        },
-        headers=headers)
-
-    return json.loads(res.text)['translations'][0]['text']
-
-
-def recognize(audio):
-    with open(audio, "rb") as f:
-        data = f.read()
-
-    params = "&".join([
-        "topic=general",
-        "folderId=%s" % FOLDER_ID,
-        "lang=ru-RU",
-        "format=lpcm",
-        "sampleRateHertz=16000"
-    ])
-
-    url = urllib.request.Request("https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?%s" % params, data=data)
-    url.add_header("Authorization", "Api-Key %s" % API_KEY)
-
-    response_data = urllib.request.urlopen(url).read().decode('UTF-8')
-    decoded_data = json.loads(response_data)
-
-    if decoded_data.get("error_code") is None:
-        result = decoded_data.get("result")
-    else:
-        result = 'Не распознал'
-
-    return result
-
-
-def main():
-    result = recognize('output.pcm')
-
-    translated_result = translate(result)
+    translated_result = synth_translator.translate(result)
 
     print(result)
     print(translated_result)
 
     with open('translated.pcm', 'wb') as f:
-        for audio_content in synthesize(text=translated_result):
+        for audio_content in synth_translator.synthesize(text=translated_result):
             f.write(audio_content)
 
     sound = AudioSegment.from_file(file='translated.pcm', sample_width=2, frame_rate=48000, channels=1)
     play(sound)
 
 
-if __name__ == '__main__':
+def main():
     synth_translator = SynthTranslator()
-    recorder = Recorder()
+    recorder = Recorder(synth_translator)
 
     recorder.record()
+
+
+if __name__ == '__main__':
+    main()
     input('конец')
