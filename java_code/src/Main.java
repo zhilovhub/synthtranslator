25106f40fbd8dc2e835f334e68d79170815f1898
Ilya Zhilov <zhilov702@gmail.com>
2022-03-05 16:12:28 +0300

    added three methods to SynthTranslator

--- java_code/src/Main.java
+++ java_code/src/Main.java
@@ -1,5 +1,11 @@
+import translator.SynthTranslator;
+
 public class Main {
     public static void main(String[] args) {
+        SynthTranslator st = new SynthTranslator();
 
+        System.out.println(st.recognize("audio"));
+        System.out.println(st.translate("text"));
+        System.out.println(st.synthesize("text"));
     }
 }
--- java_code/src/translator/SynthTranslator.java
+++ java_code/src/translator/SynthTranslator.java
@@ -1,7 +1,23 @@
 package translator;
 
 public class SynthTranslator {
-    public static void main(String[] args) {
-        System.out.println(Config.get_folder_id());
+    private final String FOLDER_ID;
+    private final String API_KEY;
+
+    public SynthTranslator() {
+        this.FOLDER_ID = Config.get_folder_id();
+        this.API_KEY = Config.get_api_key();
+    }
+
+    public String recognize(String audio) {
+        return "Recognizing finished!";
+    }
+
+    public String translate(String text) {
+        return "Translating finished!";
+    }
+
+    public String synthesize(String text) {
+        return "Synthesizing finished!";
     }
 }
