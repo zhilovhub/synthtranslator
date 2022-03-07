20ee15e30536b68c019e7f2b4830285e3720a9e3
Ilya <zhilov702@gmail.com>
2022-03-07 22:59:17 +0300

    changed tests of methods

--- java_code/src/Main.java
+++ java_code/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         SynthTranslator st = new SynthTranslator();
 
-        System.out.println(st.recognize("audio"));
+        System.out.println(st.recognize("audios/output.pcm"));
 
         System.out.println(st.translate("Здравствуйте не могли бывы подсказать как дойти до метро"));
 
