bfd26cba9dd7c9118e70a6b9eef956225d56793f
Ilya <zhilov702@gmail.com>
2022-03-07 22:58:23 +0300

    implemented recognize method

--- java_code/src/translator/SynthTranslator.java
+++ java_code/src/translator/SynthTranslator.java
@@ -27,8 +27,77 @@ public class SynthTranslator {
         this.API_KEY = Config.get_api_key();
     }
 
-    public String recognize(String audio) {
-        return "Recognizing finished!";
+    public String recognize(String audio_path) {
+        OutputStream os = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+
+        StringBuilder result = new StringBuilder();
+
+        StringJoiner params = new StringJoiner("&");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("topic", "general");
+        map.put("lang", "ru-RU");
+        map.put("format", "lpcm");
+        map.put("sampleRateHertz", "16000");
+
+        for (Map.Entry entry : map.entrySet()) {
+            params.add(entry.getKey() + "=" + entry.getValue());
+        }
+
+        try {
+            URL url_recognize = new URL("https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?" + params);
+            HttpURLConnection connection = (HttpURLConnection) url_recognize.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Api-Key " + this.API_KEY);
+            connection.setConnectTimeout(2000);
+            connection.setReadTimeout(2000);
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            os = connection.getOutputStream();
+            os.write(Files.readAllBytes(Path.of(audio_path)));
+
+            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
+                br = new BufferedReader(isr = new InputStreamReader(connection.getInputStream()));
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    result.append(line);
+                }
+            } else {
+                System.out.println(connection.getResponseCode());
+                System.out.println(connection.getResponseMessage());
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e);
+        } finally {
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException ignored) {
+                    System.out.println("Error: " + ignored);
+                }
+            }
+            if (isr != null) {
+                try {
+                    isr.close();
+                } catch (IOException ignored) {
+                    System.out.println("Error: " + ignored);
+                }
+            }
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ignored) {
+                    System.out.println("Error: " + ignored);
+                }
+            }
+        }
+
+        return result.toString();
     }
 
     public String translate(String text) {
