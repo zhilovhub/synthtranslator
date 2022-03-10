88f16383c1c728b317d71f68f868b96e622b3403
Ilya Zhilov <zhilov702@gmail.com>
2022-03-10 19:39:59 +0300

    capturing audio for 2 seconds and play it

--- java_code/src/recorder/VoiceRecorder.java
+++ java_code/src/recorder/VoiceRecorder.java
@@ -3,22 +3,49 @@ package recorder;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.AudioInputStream;
 
 import translator.SynthTranslator;
 
 import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.IOException;
 
 public class VoiceRecorder {
     private final SynthTranslator synth_translator;
     private final AudioFormat audio_format = get_audio_format();
+    private AudioInputStream audio_stream;
     private TargetDataLine target_data_line;
-    private final int seconds = 1;
+    private SourceDataLine source_data_line;
+    private ByteArrayOutputStream output_stream;
+    private final int seconds = 2;
 
     public VoiceRecorder(SynthTranslator synth_translator) {
         this.synth_translator = synth_translator;
+    }
+
+    private void play_audio() {
+        try {
+            byte audio_data[] = output_stream.toByteArray();
+
+            InputStream input_stream = new ByteArrayInputStream(audio_data);
+            audio_stream = new AudioInputStream(input_stream, audio_format, audio_data.length / this.audio_format.getFrameSize());
+
+            DataLine.Info data_line_info = new DataLine.Info(SourceDataLine.class, audio_format);
+            source_data_line = (SourceDataLine) AudioSystem.getLine(data_line_info);
+
+            source_data_line.open(audio_format);
+            source_data_line.start();
+
+            Player player = new Player();
+            player.start();
+        } catch (LineUnavailableException e) {
+            System.out.println("Error: " + e);
+        }
     }
 
     public void capture_audio() {
@@ -26,10 +53,10 @@ public class VoiceRecorder {
             DataLine.Info data_line_info = new DataLine.Info(TargetDataLine.class, this.audio_format);
             this.target_data_line = (TargetDataLine) AudioSystem.getLine(data_line_info);
 
-            Capturer capturer = new Capturer();
-
             this.target_data_line.open(this.audio_format);
             this.target_data_line.start();
+
+            Capturer capturer = new Capturer();
 
             capturer.start();
 
@@ -48,10 +75,29 @@ public class VoiceRecorder {
         );
     }
 
+    private final class Player extends Thread {
+        public void run() {
+            byte[] temp_buffer = new byte[1024 * audio_format.getChannels() * audio_format.getFrameSize()];
+            int cnt;
+
+            try {
+                while ((cnt = audio_stream.read(temp_buffer, 0, temp_buffer.length)) != -1) {
+                    source_data_line.write(temp_buffer, 0, cnt);
+                }
+
+                source_data_line.drain();
+                source_data_line.close();
+
+            } catch (IOException e) {
+                System.out.println("Error: " + e);
+            }
+        }
+    }
+
     private final class Capturer extends Thread {
         public void run() {
             byte[] temp_buffer = new byte[1024 * audio_format.getChannels() * audio_format.getFrameSize()];
-            ByteArrayOutputStream output_stream = new ByteArrayOutputStream();
+            output_stream = new ByteArrayOutputStream();
             int cnt;
 
             while ((cnt = target_data_line.read(temp_buffer, 0, temp_buffer.length)) != -1) {
@@ -64,7 +110,11 @@ public class VoiceRecorder {
             }
 
             try {
+                target_data_line.stop();
+                target_data_line.drain();
+                target_data_line.close();
                 output_stream.close();
+                play_audio();
             } catch (IOException ignored) {
                 System.out.println("Error: " + ignored);
             }
