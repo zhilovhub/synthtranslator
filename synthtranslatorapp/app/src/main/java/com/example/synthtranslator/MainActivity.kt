a9f9035b947b26c177eb882f28bb8747950cbc2b
Ilya Zhilov <zhilov702@gmail.com>
2022-04-02 19:51:42 +0300

    added databinding

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.synthtranslator
 
+import androidx.databinding.DataBindingUtil;
+import com.example.synthtranslator.databinding.ActivityMainBinding;
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
