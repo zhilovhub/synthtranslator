39d20bd2f241c5d9eec7a83290b8c6166b501661
Ilya Zhilov <zhilov702@gmail.com>
2022-04-02 21:33:40 +0300

    translate test text

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.synthtranslator
 
-import androidx.databinding.DataBindingUtil;
-import com.example.synthtranslator.databinding.ActivityMainBinding;
+import com.example.synthtranslator.translator.SynthTranslator
+
+import androidx.databinding.DataBindingUtil
+import com.example.synthtranslator.databinding.ActivityMainBinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val st = SynthTranslator()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.startButton.setOnClickListener {
+            translateText("Привет, у меня есть проблема! Как дайти до метро")
+        }
+    }
+
+    private fun translateText(text: String) {
+        println(st.execute(text))
     }
 }
