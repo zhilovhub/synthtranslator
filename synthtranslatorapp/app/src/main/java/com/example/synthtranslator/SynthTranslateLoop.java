de1b474cc293f51b48a23cdd70b4dad39cb10001
Ilya Zhilov <zhilov702@gmail.com>
2022-04-03 12:08:33 +0300

    init SynthTranslateLoop

---
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SynthTranslateLoop.java
@@ -0,0 +1,12 @@
+package com.example.synthtranslator;
+
+import android.os.AsyncTask;
+
+class SynthTranslatorLoop extends AsyncTask {
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+
+}
