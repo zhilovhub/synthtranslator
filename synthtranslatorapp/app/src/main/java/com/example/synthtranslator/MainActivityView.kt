6281fa3ee86b4d792dcb240e271c2d4e6d653765
Ilya Zhilov <zhilov702@gmail.com>
2022-04-03 21:31:10 +0300

    added coroutine

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivityView.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivityView.kt
@@ -2,13 +2,33 @@ package com.example.synthtranslator
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.synthtranslator.translator.SynthTranslator
+import kotlinx.coroutines.*
 
 class MainActivityView : ViewModel() {
     init {
         Log.i("MainActivityView", "Model created!")
     }
 
+    private var viewModelJob = Job()
+    private var uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+    private var result: String = ""
+
     fun translateText(text: String) {
-        println(SynthTranslatorLoop().execute(text).toString())
+        uiScope.launch {
+            result = suspendTranslateText(text)
+        }
+    }
+
+    private suspend fun suspendTranslateText(text: String): String {
+        return withContext(Dispatchers.IO) {
+            val resultValue = SynthTranslator().translate(text)
+            resultValue
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SynthTranslatorLoop.java
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SynthTranslatorLoop.java
@@ -1,21 +1,15 @@
 package com.example.synthtranslator;
 
-import android.os.AsyncTask;
 import java.io.InputStream;
 
 import com.example.synthtranslator.translator.SynthTranslator;
 
-class SynthTranslatorLoop extends AsyncTask {
+class SynthTranslatorLoop {
     private final SynthTranslator st = new SynthTranslator();
 
     private String recognized_text;
     private String translated_text;
     private InputStream synthesized_stream;
 
-    @Override
-    protected Object doInBackground(Object[] objects) {
-        translated_text = st.translate(objects[0].toString());
-        return null;
-    }
 
 }
