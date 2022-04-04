910b68c1532b4dfe1fca3a8a7610afec579234fe
Ilya Zhilov <zhilov702@gmail.com>
2022-04-04 17:12:20 +0300

    testFunction for recording

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.synthtranslator
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
 import androidx.databinding.DataBindingUtil
 import com.example.synthtranslator.databinding.ActivityMainBinding
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,7 +28,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         binding.startButton.setOnClickListener {
-            viewModel.translateText("Привет, у меня есть проблема! Как дайти до метро")
+//            viewModel.translateText("Привет, у меня есть проблема! Как дайти до метро")
+            testFunction()
+        }
+    }
+
+    private fun testFunction() {
+        val minBufferSize = AudioRecord.getMinBufferSize(16000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT)
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 1234)
+        } else {
+            val recorder = AudioRecord(MediaRecorder.AudioSource.MIC, 16000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize)
+            recorder.startRecording()
         }
     }
 }
