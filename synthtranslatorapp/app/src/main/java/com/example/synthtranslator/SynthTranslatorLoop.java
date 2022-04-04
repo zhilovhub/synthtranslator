0265a6c7a735b5e2a96ed75924ca4b5267da70f6
Ilya Zhilov <zhilov702@gmail.com>
2022-04-04 17:11:54 +0300

    added VoiceRecorder (vr) to Loop

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SynthTranslatorLoop.java
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SynthTranslatorLoop.java
@@ -3,9 +3,11 @@ package com.example.synthtranslator;
 import java.io.InputStream;
 
 import com.example.synthtranslator.translator.SynthTranslator;
+import com.example.synthtranslator.recorder.VoiceRecorder;
 
 class SynthTranslatorLoop {
     private final SynthTranslator st = new SynthTranslator();
+    private final VoiceRecorder vr = new VoiceRecorder();
 
     private String recognized_text;
     private String translated_text;
