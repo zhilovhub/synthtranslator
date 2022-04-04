8bbf0771136d80fc80c80f212e7a57bb19bda50d
Ilya Zhilov <zhilov702@gmail.com>
2022-04-04 17:11:13 +0300

    initilazied VoiceRecorder

---
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/recorder/VoiceRecorder.java
@@ -0,0 +1,10 @@
+package com.example.synthtranslator.recorder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class VoiceRecorder {
+
+}
