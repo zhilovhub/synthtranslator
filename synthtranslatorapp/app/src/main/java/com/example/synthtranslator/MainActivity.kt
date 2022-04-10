0435fd331f7558a07bf2d13ee436fa2a2c29fee3
Ilya <zhilov702@gmail.com>
2022-04-10 10:33:39 +0300

    properly working permission system

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
@@ -7,17 +7,22 @@ import android.media.AudioRecord
 import android.media.AudioTrack
 import android.media.MediaRecorder
 import android.media.AudioManager
-import androidx.databinding.DataBindingUtil
+
 import com.example.synthtranslator.databinding.ActivityMainBinding
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.core.app.ActivityCompat
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityView
+    private lateinit var toast: Toast
     private var hasCreated: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,31 +30,68 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainActivityView::class.java)
+        toast = Toast.makeText(this, "Мы не можем записывать голос без разрешения на использование микрофона", Toast.LENGTH_SHORT)
 
         setListeners()
     }
 
     private fun setListeners() {
-        binding.startButton.setOnClickListener {
-            recordingLoop()
+        binding.startButton.setOnClickListener {view: View ->
+            recordingLoop(view)
+        }
+
+        binding.stopButton.setOnClickListener {view: View ->
+            stopLoop(view)
         }
     }
 
-    private fun recordingLoop() {
+    private fun recordingLoop(view: View) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 1234)
-        } else if (!hasCreated) {
-            val minBufferSizeRecording = AudioRecord.getMinBufferSize(16000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT)
-            val recorder = AudioRecord(MediaRecorder.AudioSource.MIC, 16000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, minBufferSizeRecording * 4)
-
-            val minBufferSizePlaying = AudioTrack.getMinBufferSize(48000, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
-            val player = AudioTrack(AudioManager.STREAM_MUSIC, 48000, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, minBufferSizePlaying * 4, AudioTrack.MODE_STREAM)
-
-            viewModel.setAudioRecorder(recorder, player)
-            hasCreated = true
-            viewModel.startRecording()
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+                toast.show()
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 1234)
+            }
         } else {
+            if (!hasCreated) {
+                val minBufferSizeRecording = AudioRecord.getMinBufferSize(
+                    16000,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT
+                )
+                val recorder = AudioRecord(
+                    MediaRecorder.AudioSource.MIC,
+                    16000,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    minBufferSizeRecording * 4
+                )
+
+                val minBufferSizePlaying = AudioTrack.getMinBufferSize(
+                    48000,
+                    AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT
+                )
+                val player = AudioTrack(
+                    AudioManager.STREAM_MUSIC,
+                    48000,
+                    AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    minBufferSizePlaying * 4,
+                    AudioTrack.MODE_STREAM
+                )
+
+                viewModel.setAudioInstruments(recorder, player)
+                hasCreated = true
+            }
             viewModel.startRecording()
+            view.isEnabled = false
+            binding.stopButton.isEnabled = true
         }
+    }
+
+    private fun stopLoop(view: View) {
+        view.isEnabled = false
+        binding.startButton.isEnabled = true
     }
 }
--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivityView.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivityView.kt
@@ -2,61 +2,37 @@ package com.example.synthtranslator
 
 import android.media.AudioRecord
 import android.media.AudioTrack
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.example.synthtranslator.translator.SynthTranslator
-import kotlinx.coroutines.*
-import java.io.IOException
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivityView : ViewModel() {
     private var viewModelJob = Job()
     private var uiScope = CoroutineScope(Dispatchers.Default + viewModelJob)
-    private lateinit var recorder: AudioRecord
-    private lateinit var player: AudioTrack
-    private lateinit var synthTranslatorLoop: SynthTranslatorLoop
+    private var recorder: AudioRecord? = null
+    private var player: AudioTrack? = null
+    private var synthTranslatorLoop: SynthTranslatorLoop? = null
 
     fun startRecording() {
         uiScope.launch {
-            synthTranslatorLoop.startLoop()
+            synthTranslatorLoop?.startLoop()
         }
     }
 
-    fun setAudioRecorder(recorder: AudioRecord, player: AudioTrack) {
+    fun setAudioInstruments(recorder: AudioRecord, player: AudioTrack) {
         this.recorder = recorder
         this.player = player
         synthTranslatorLoop = SynthTranslatorLoop(recorder, player)
     }
 
-//    private suspend fun suspendStartRecording() {
-//        return withContext(Dispatchers.Default) {
-//            synthTranslatorLoop.startLoop()
-//        }
-//    }
-
-//    private suspend fun suspendStartRecording() {
-//        return withContext(Dispatchers.IO) {
-//            val temp_buffer = ByteArray(1024)
-//            var cnt: Int
-//
-//            var input_stream = SynthTranslator().synthesize("Hello I am testing my program. I should work for a long time")
-//
-//            try {
-//                while (true) {
-//                    if (input_stream != null) {
-//                        cnt = input_stream.read(temp_buffer, 0, temp_buffer.size)
-//                        if (cnt != -1) {
-//                            player.write(temp_buffer, 0, cnt)
-//                            println(1)
-//                        }
-//                    }
-//                }
-//            } catch (e: IOException) {
-//                println("Error: $e")
-//            }
-//        }
-//    }
-
     override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
+        player?.release()
+        recorder?.release()
+        Log.i("MainActivityView", "OnCleared!!!!!")
     }
 }
