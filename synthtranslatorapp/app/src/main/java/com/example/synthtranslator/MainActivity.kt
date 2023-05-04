6a4c6c32bab07882789a241130423157ad0f6147
Ilya Zhilov <zhilovgithub@gmail.com>
2023-05-04 16:59:45 +0300

    setupActionBarWithNavController

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
@@ -2,16 +2,26 @@ package com.example.synthtranslator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
-
-
-
+    override fun onNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onNavigateUp()
+    }
 
 
 }
