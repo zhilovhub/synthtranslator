3ce7306ea07b1053fadf4b21916738a20cc1afef
Ilya Zhilov <zhilovgithub@gmail.com>
2023-05-05 17:59:09 +0300

    drawerLayout init

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
@@ -2,20 +2,27 @@ package com.example.synthtranslator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.synthtranslator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        val drawerLayout = binding.drawerLayout
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
     }
 
     override fun onNavigateUp(): Boolean {
