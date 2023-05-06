d1c9a3f70ac78d7e5e3bc82fae242cd1b9677cbf
Ilya <zhilovgithub@gmail.com>
2023-05-06 21:34:30 +0300

    SettingsFragment init

---
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SettingsFragment.kt
@@ -0,0 +1,22 @@
+package com.example.synthtranslator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.synthtranslator.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+
+        return binding.root
+    }
+}
