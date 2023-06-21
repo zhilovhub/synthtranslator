b7cfb3cf697f2f352661555e6f24c979f873d553
Ilya Zhilov <zhilovgithub@gmail.com>
2023-06-21 09:47:11 +0300

    moveToSettingFragment

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.synthtranslator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -10,15 +11,16 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.synthtranslator.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
     lateinit var appBarConfiguration: AppBarConfiguration
     lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.nav_host_fragment)
         val drawerLayout = binding.drawerLayout
 
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
@@ -27,7 +29,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(binding.drawerLayout) || super.onSupportNavigateUp()
     }
 }
