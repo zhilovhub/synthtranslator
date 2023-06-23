003938baec3cba63e24787fc5d8262101c9a0f81
Ilya Zhilov <zhilovgithub@gmail.com>
2023-06-23 13:28:09 +0300

    seekBar.setOnSeekBarChangeListener

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SettingsFragment.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SettingsFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.databinding.DataBindingUtil
 import com.example.synthtranslator.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
+
+    private var lastDBGroup = 6
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +20,20 @@ class SettingsFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
 
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                changeDBBarrier(p1)
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+        })
+
         return binding.root
+    }
+
+    private fun changeDBBarrier(newDB: Int) {
+        binding.decibelsTextview.text = "${newDB + 14} Дб"
     }
 }
