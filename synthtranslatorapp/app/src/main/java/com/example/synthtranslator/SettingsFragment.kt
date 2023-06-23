40ca499de8031d65eb210fc1b34c940bdcb0588d
Ilya Zhilov <zhilovgithub@gmail.com>
2023-06-23 13:43:05 +0300

    decibels_fact_textview

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SettingsFragment.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SettingsFragment.kt
@@ -33,7 +33,29 @@ class SettingsFragment : Fragment() {
         return binding.root
     }
 
-    private fun changeDBBarrier(newDB: Int) {
-        binding.decibelsTextview.text = "${newDB + 14} Дб"
+    private fun changeDBBarrier(DB: Int) {
+        val newDB = DB + 10
+
+        if (newDB in 100..120 && lastDBGroup != 5) {
+            binding.decibelsFactTextview.text = "Отбойный молоток"
+            lastDBGroup = 5
+        } else if (newDB in 80..99 && lastDBGroup != 4) {
+            binding.decibelsFactTextview.text = "Шум московского метро"
+            lastDBGroup = 4
+        } else if (newDB in 60..79 && lastDBGroup != 3) {
+            binding.decibelsFactTextview.text = "Мотоцикл с глушителем"
+            lastDBGroup = 3
+        } else if (newDB in 40..59 && lastDBGroup != 2) {
+            binding.decibelsFactTextview.text = "Пылесос"
+            lastDBGroup = 2
+        } else if (newDB in 20..39 && lastDBGroup != 1) {
+            binding.decibelsFactTextview.text = "Тихий офис"
+            lastDBGroup = 1
+        } else if (newDB in 10..19 && lastDBGroup != 0) {
+            binding.decibelsFactTextview.text = "Шелест листьев"
+            lastDBGroup = 0
+        }
+
+        binding.decibelsTextview.text = "$newDB Дб"
     }
 }
