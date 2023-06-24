762c46896cd8e8dae2756c9fce90bd180c63d827
Ilya Zhilov <zhilovgithub@gmail.com>
2023-06-24 12:32:29 +0300

    SettingsFragmentViewModel

--- synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SettingsFragment.kt
+++ synthtranslatorapp/app/src/main/java/com/example/synthtranslator/SettingsFragment.kt
@@ -1,16 +1,34 @@
 package com.example.synthtranslator
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.synthtranslator.databinding.FragmentSettingsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
+    private lateinit var viewModel: SettingsFragmentViewModel
+    private lateinit var toast: Toast
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+
+    private lateinit var recorder: AudioRecord
 
     private var lastDBGroup = 6
 
@@ -19,6 +37,10 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+        viewModel = ViewModelProvider(this)[SettingsFragmentViewModel::class.java]
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
+        toast = Toast.makeText(activity, "Мы не можем записывать голос без разрешения на использование микрофона", Toast.LENGTH_SHORT)
 
         binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
@@ -67,5 +89,48 @@ class SettingsFragment : Fragment() {
 
         binding.decibelsTextview.text = "$newDB Дб"
         binding.decibelsFactImageview.setImageResource(imageResource)
+    }
+
+    private fun setAudioInstruments() {
+        if (context?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.RECORD_AUDIO) } == PackageManager.PERMISSION_GRANTED) {
+            val minBufferSizeRecording = AudioRecord.getMinBufferSize(
+                16000,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT
+            )
+            recorder = AudioRecord(
+                MediaRecorder.AudioSource.MIC,
+                16000,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                minBufferSizeRecording * 4
+            )
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println(1)
+
+        if (view?.let { ContextCompat.checkSelfPermission(it.context, Manifest.permission.RECORD_AUDIO) } != PackageManager.PERMISSION_GRANTED) {
+            println(2)
+            if (shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO)) {
+                toast.show()
+            } else {
+                permissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
+            }
+        } else {
+            println(3)
+            setAudioInstruments()
+            viewModel.startRecording(recorder)
+        }
+
+        println("onResume")
+    }
+
+    override fun onStop() {
+        println("onStop")
+        viewModel.stopRecording()
+        super.onStop()
     }
 }
